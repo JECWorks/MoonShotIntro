fcfe6fd1b624808d20084df8299d3c3174fdac4a
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-11-21 21:29:11 -0500

    Initial Commit

---
+++ MoonShotIntro/MoonShotIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotIntroApp.swift
+//  MoonShotIntro
+//
+//  Created by Jason Cox on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
